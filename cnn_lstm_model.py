a605e7665f1bbff6844b091f8060c9a4f02c7266
Marc <81183798+marchcarax@users.noreply.github.com>
2021-04-23 12:30:06 +0200

    model

--- cnn_lstm_model.py
+++ cnn_lstm_model.py
@@ -24,9 +24,9 @@ class model():
                         kernel_size=2, strides=1, activation='relu', padding='same'))
         model.add(layers.Conv1D(filters=64, kernel_size=2, strides=1,
                         activation='relu', padding='same'))
-        model.add(layers.LSTM(1000, return_sequences=True))
+        model.add(layers.LSTM(300, return_sequences=True))
         model.add(layers.Dropout(0.2))
-        model.add(layers.LSTM(500,  return_sequences=False))
+        model.add(layers.LSTM(200,  return_sequences=False))
         model.add(layers.Dropout(0.2))
         model.add(layers.Dense(100, kernel_initializer='uniform', activation='relu'))
         model.add(layers.Dense(1, kernel_initializer='uniform', activation='relu'))
