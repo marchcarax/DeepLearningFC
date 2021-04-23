fcdfa4900856f69987ff79a5a4d534c688e3a579
Marc <81183798+marchcarax@users.noreply.github.com>
2021-04-23 20:09:00 +0200

    main

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def main():
     y_test = data.scy.inverse_transform(y_test.reshape(-1,1))
     res = data.scy.inverse_transform(lstm_model.results)
     forecast = data.scy.inverse_transform(lstm_model.future)
-    
+
     plot.plot_test(data.date, data.split, y_test, res)
-    plot.plot_future(forecast, 5)
+    df_predict = plot.plot_future(forecast, 5)
+    plot.plot_label(df_predict)
 
     '''
     next steps:
@@ -25,13 +26,20 @@ def main():
         create a predict function or file that test the data --> DONE
         inverse transform here --> DONE
         plot the results --> DONE
-        forecast next possible periods --> DONE??? create a simulation of 5 possible FC
-        label prediction with custom indicator
+        forecast next possible periods --> DONE (???) 
+        add X_fut --> DONE
+        leave y(close) outside x and see how it performs --> DONE, NOT BAD :D
+        create a simulation of 5 possible FC . It would need to be trained 5 times to work...it takes toomuch time --> NO
+        add ci in test plot --> NO, not worth it
+        label prediction with custom indicator with np.where =1 UP, =0 DOWN, or range --> DONE
+        NOT WORKING WELL THE FC WHYYYYYYYYY
+        do the label pred with full trained and pred data!! not close!
 
         future:
         new mix in layers
         try with my custom indicator
         add compatibility with other systems like xgb and hmm
+        add new models for 1min (or 15m) eurusd and 1D rolling analysis as my excel model
     '''
 
 if __name__ == '__main__':
